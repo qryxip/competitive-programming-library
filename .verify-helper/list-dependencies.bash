dd557cca67f9091644c73ce026e52ce7ceffef5c
Ryo Yamashita <qryxip@gmail.com>
2020-09-02 22:06:47 +0900

    Find the library location

--- .verify-helper/list-dependencies.bash
+++ .verify-helper/list-dependencies.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
-LIB_NAME=lib
-lib_manifest_dir="$(realpath "$(dirname "$0")/..")/$LIB_NAME"
 src_path="$(realpath "$1")"
 cd "$(dirname "$src_path")" || exit 1
 if [ "$(cargo metadata --format-version 1 --no-deps | jq --arg src_path "$src_path" '.packages[].targets[] | select(.src_path == $src_path) | .kind == ["bin"]')" == true ]; then
-  printf '%s' "$(cargo equip --src "$src_path" || exit 1)" | sed -nr 's;^//! - `([a-zA-Z0-9_]+)::([a-zA-Z0-9_]+)` → .*$;\2;p' | xargs -rn 1 printf "%s/src/%s.rs\n" "$lib_manifest_dir"
+  bundled="$(cargo equip --check --src "$src_path")" || exit 1
+  bin_pkg_name="$(cargo metadata --format-version 1 --no-deps | jq -r --arg src_path "$src_path" '.packages[] | select(.targets | any(.src_path == $src_path)) | .name')" || exit 1
+  lib_manifest_path="$(cargo metadata --format-version 1 | jq -r --arg bin_pkg_name "$bin_pkg_name" '.packages[] | select(.source == null and .name != $bin_pkg_name) | .manifest_path')" || exit 1
+  printf '%s' "$bundled" | sed -nr 's;^//! - `([a-zA-Z0-9_]+)::([a-zA-Z0-9_]+)` → .*$;\2;p' | xargs -rn 1 printf "%s/src/%s.rs\n" "$(dirname "$lib_manifest_path")"
 elif [ "$(basename "$src_path")" = lib.rs ]; then
   false # How do I exclude `lib.rs`?
 else
