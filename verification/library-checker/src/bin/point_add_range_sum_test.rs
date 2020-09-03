874355563430a0b364b659dd53e6da1234018fab
Ryo Yamashita <qryxip@gmail.com>
2020-09-04 04:14:32 +0900

    Bump `cargo-equip` to v0.3.0

--- verification/library-checker/src/bin/point_add_range_sum_test.rs
+++ verification/library-checker/src/bin/point_add_range_sum_test.rs
@@ -1,6 +1,6 @@
 // verify-helper: PROBLEM https://judge.yosupo.jp/problem/point_add_range_sum
 
-#[::cargo_equip::equip]
+#[cfg_attr(cargo_equip, cargo_equip::equip)]
 use ::__lib::{fenwick::AdditiveFenwickTree, input, output};
 
 use std::io::Write as _;
