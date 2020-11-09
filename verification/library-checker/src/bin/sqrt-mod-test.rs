fab02a4cd1e60919c93c0d86e144e652e87276f9
Ryo Yamashita <qryxip@gmail.com>
2020-11-09 21:11:36 +0900

    Add a solution for `sqrt_mod` on Library-Checker

---
+++ verification/library-checker/src/bin/sqrt-mod-test.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate input as _;
+
+use acl_modint::ModInt;
+use std::io::Write as _;
+use tonelli_shanks::ModIntBaseExt as _;
+
+fn main() {
+    input! {
+        yps: [(u32, u32)],
+    }
+
+    output::buf_print(|out| {
+        macro_rules! println(($($tt:tt)*) => (writeln!(out, $($tt)*).unwrap()));
+        for (y, p) in yps {
+            ModInt::set_modulus(p);
+            if let Some(sqrt) = ModInt::new(y).sqrt() {
+                println!("{}", sqrt);
+            } else {
+                println!("-1");
+            }
+        }
+    });
+}
