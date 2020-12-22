f67248c3d83f8491488cabab58418ade4250cfd5
Ryo Yamashita <qryxip@gmail.com>
2020-12-22 18:11:14 +0900

    Update `sqrt-mod` to use `fastout`

--- verification/library-checker/src/bin/sqrt-mod.rs
+++ verification/library-checker/src/bin/sqrt-mod.rs
@@ -1,29 +1,25 @@
 // verify-helper: PROBLEM https://judge.yosupo.jp/problem/sqrt_mod
 
-// Uncomment this line if you don't use your libraries. (`--check` still works)
-//#![cfg_attr(cargo_equip, cargo_equip::skip)]
-
+#[macro_use]
+extern crate fastout as _;
 #[macro_use]
 extern crate input as _;
 
 use acl_modint::ModInt;
-use std::io::Write as _;
 use tonelli_shanks::ModIntBaseExt as _;
 
+#[fastout]
 fn main() {
     input! {
         yps: [(u32, u32)],
     }
 
-    buffered_print::buf_print(|out| {
-        macro_rules! println(($($tt:tt)*) => (writeln!(out, $($tt)*).unwrap()));
-        for (y, p) in yps {
-            ModInt::set_modulus(p);
-            if let Some(sqrt) = ModInt::new(y).sqrt() {
-                println!("{}", sqrt);
-            } else {
-                println!("-1");
-            }
+    for (y, p) in yps {
+        ModInt::set_modulus(p);
+        if let Some(sqrt) = ModInt::new(y).sqrt() {
+            println!("{}", sqrt);
+        } else {
+            println!("-1");
         }
-    });
+    }
 }
